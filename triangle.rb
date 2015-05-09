825027114843271cc23357de33536a68f06c8cc3
Chris Craig <ccraig88@gmail.com>
2015-05-09 09:59:13 -0400

    got triangle2

--- triangle.rb
+++ triangle.rb
@@ -12,8 +12,17 @@
 #   about_triangle_project.rb
 # and
 #   about_triangle_project_2.rb
-#
+
+def test_input(a,b,c)
+  if a < 1 or b<1 or c<1 then
+    raise TriangleError, "Sides should be positive integers"
+  elsif (a+b <= c) or (b+c <= a) or (c+a <= b)
+    raise TriangleError, "The sum of two legs should be greater than the hypotenuse"
+  end
+end
+
 def triangle(a, b, c)
+  test_input(a,b,c)
   if a.equal? b and b.equal? c then
       return :equilateral
   elsif a.equal? b or b.equal? c or c.equal? a then
